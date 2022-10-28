b08b599f83eba5bac53a5e235ed12ed5f1b24524
amirfzd <amiralifarazmand80@gmail.com>
2022-10-28 18:30:15 +0330

    question 3 code got a little bit cleaner. end of exercise.----------------------------------------------

--- exc1/xshr.c
+++ exc1/xshr.c
@@ -47,40 +47,13 @@ int infix_eval(int numbers[50],char op[50])
     {
         case ',':
             return x+y;
-        case '-':
-            return y-x;
-        case '*':
-            return x*y;
-        case '/':
-            return y/x;
-        case '{':
-            printf("{ was seen !\n") ; 
-            push_num(y);
-            return x ;
     }
     return 0;
 }
 //function to check whether the character is an operator or not
 int is_operator(char ch)
 {
-    return(ch==','||ch=='-'||ch=='*'||ch=='/');
-}
-//function that declear the precedence of operators
-int precedence(char c)
-{
-    switch(c)
-    {
-        case ',':
-        return 1;
-        case '-':
-        return 1;
-        case '*':
-        return 2;
-        case '/':
-        return 2;
-        default:
-        return 0;
-    }
+    return(ch==',');
 }
 //function to evaluate an infix expression
 int eval(char exp[20])
@@ -91,8 +64,7 @@ int eval(char exp[20])
     {
         c = exp[i];
         
-        if (c== ' ' ||isspace(c)==1){
-            // printf("decleared whitespace\n");
+        if (c== ' ' || isspace(c)==1){
             continue;
         }
         if(isdigit(c)!=0)
@@ -109,20 +81,17 @@ int eval(char exp[20])
             }
             i--;
             push_num(num);
-            // printf("______num:%d was here\n" , num);
         }
         
         else if(c=='{')
         {
             push_op(c);
             continue;
-            // printf("______{ was here\n");
         }
 
         else if(c=='}')
         {
             int more_than_one_flag = 0 ; 
-            // printf("______} was here\n");
             while(op[to]!='{' )
             {
                 more_than_one_flag =1 ; 
@@ -133,56 +102,26 @@ int eval(char exp[20])
                 r = pop_num() ;
                 push_num(r) ;
             }
-            // r = infix_eval(numbers, op);
-            // push_num(r);
             pop_op();
-            // printf("****************r: %d\n" , r);
             printf("%d\n" , r);
-            // printf("***r:%d\t%d\n"  ,r , numbers[tn--]);
         }
-        // the current character is operator for sure
+
         else if(is_operator(c))
         {
-            // while(to!=-1 && precedence(c)<=precedence(op[to]))
-            // {
-            //     output = infix_eval(numbers, op);
-            //     push_num(output);
-            // }
             push_op(c);
-            // printf("______op:%c was here\n", c);
-
         }
+    }
+}   
 
-    }
-    //if there\n is any remaining expression, evaluate them
-    while(to!=-1)
-    {
-        output = infix_eval(numbers, op);
-        push_num(output);
-    }
-    return pop_num();
-}
+
 int main()
 {   
     int n=1, i, high, aux;
-    // char exp[10][50];
-    // int res[10];
-    char input[50];
+    char input[100];
     char result[50];
-
-    // }
-    // scanf("%s" , input);
-    fgets (input, 100, stdin);
+    // fgets (input, 100, stdin);
+    scanf("%[^\n]s" , input) ; 
     eval(input);
-    // for(i=0; i<n; i++){
-    //     high = max(res, i, n-1);
-    //     aux = res[i];
-    //     res[i] = res[high];
-    //     res[high] = aux;
-    // }
-    // for (i=0; i<n; i++){
-    //     printf("%d ",res[i]);
-    // }
-    printf("\n");
+    // printf("\n");
     return 1;
-}
+}    
