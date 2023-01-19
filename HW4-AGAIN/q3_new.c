7a8987a98240c1c0218631c7fa0ad8b16af5ef11
amirfzd <amiralifarazmand80@gmail.com>
2023-01-19 16:07:05 +0330

    q3_new is all done!

--- HW4-AGAIN/q3_new.c
+++ HW4-AGAIN/q3_new.c
@@ -1,15 +1,17 @@
 /*
 Useful links:
     https://www.geeksforgeeks.org/pipe-system-call/
+    https://www.geeksforgeeks.org/c-program-demonstrate-fork-and-pipe/
     https://stackoverflow.com/questions/28289157/c-multiprocessing-pipes
 
 */
 
-#include <stdio.h>
+
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
 #define PROC_COUNT 7
-int main(void)
+int main(char **argv)
 {
     int fd[PROC_COUNT][2];
     int pid[PROC_COUNT];
@@ -22,15 +24,15 @@ int main(void)
         if ( pid[i] == 0 ) {
             close(fd[i][0]);
 
-            char arr[100];
-            sprintf(arr, "%d", i + 1);
-            write(fd[i][1], arr, 1) ;
+            char in_a[100];
+            sprintf(in_a, "%d", i + 1);
+            write(fd[i][1], in_a, 1) ;
 
             close(fd[i][1]) ;
 
             return 0;
         }
-        else {
+        else { // pid[i] == 0
             close(fd[i][1]) ;
             }
     }
@@ -38,13 +40,12 @@ int main(void)
     for ( int i = 0; i < PROC_COUNT; ++i ) {
         waitpid(pid[i], NULL, 0);
 
-        char out[100] = {0};
-        read(fd[i][0], out, 99);
+        char output[100] = {0};
+        read(fd[i][0], output, 99);
 
-        printf("log of process:\t%s\n\n", out);
+        printf("log of process:\t%s\n\n", output);
 
         close(fd[i][0]);
     }
-
     return 0;
 }
