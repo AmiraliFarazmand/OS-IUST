35e98f314aa7d2354f4f930895996e1ebe87b28e
amirfzd <amiralifarazmand80@gmail.com>
2023-01-20 07:56:08 +0330

    shebaahat bala ba arian

--- HW4-AGAIN/kossher.c
+++ HW4-AGAIN/kossher.c
@@ -1,6 +1,10 @@
 /*
 Useful links:
     https://users.cs.cf.ac.uk/Dave.Marshall/C/node27.html
+    https://stackoverflow.com/questions/5656530/how-to-use-shared-memory-with-linux-in-c
+    https://man7.org/linux/man-pages/man3/shmat.3p.html
+    https://www.geeksforgeeks.org/c-program-for-merge-sort/
+    https://github.com/cave7/sort/blob/master/mergesort.c
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,15 +26,21 @@ Useful links:
 #include <sys/shm.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
-#define ARRAY_SIZE 4
+#include <sys/types.h>
+#include <sys/ipc.h> 
+#include <sys/shm.h> 
+#include <sys/wait.h>
+#define ARRAY_SIZE 6
 
-int arr [4] = {4665, 5, 44 ,8} ;
+int arr [ARRAY_SIZE] = {4, 2 , 3, 2 , 0 ,9} ;
      
 int * shm ;
-int  shm_seg ;
+int  shm_segID ;
 int temp_arr[100] ;
 void merge_sort(int l , int r);
-void merge(int s1 , int e1 , int s2 , int e2) ;
+void merge(int s1 , int e1 , int e2) ;
+int jj;
+
 
 void printArray(int A[], int size)
 {
@@ -39,26 +49,69 @@ void printArray(int A[], int size)
         printf("%d ", A[i]);
     printf("\n");
 }
+// -------------------------------------------------------------------------------------------------
 // =================================================================================================
 void main(){
     // int arr[ARRAY_SIZE]= {0,0,0,0} ;
     // int* arr  =(int*) malloc(ARRAY_SIZE* sizeof(int));
     // arr = (int*) mmap(NULL, sizeof(int)* ARRAY_SIZE, PROT_READ| PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, 0, 0);
     // if (arr=-1){printf("shit\n");return 1;}
-    printArray(arr, ARRAY_SIZE);
-    int *ptr = mmap(NULL,ARRAY_SIZE*sizeof(int),
-     PROT_READ | PROT_WRITE,
-     MAP_SHARED | MAP_ANONYMOUS,
-     0,0);    
+    // printArray(arr, ARRAY_SIZE);
+    pid_t pid,pid2;
+    struct shmid_ds item;
+    int wait1,wait2;
+    int shmid; /* return value from shmget() */ 
+    int* shm_atch ; 
+    shmid = shmget(IPC_PRIVATE , 1000*sizeof(int) , IPC_CREAT|0666);
+    if (shmid < 0){printf("error in shmget()") ; exit(1);}
 
-    if(ptr == MAP_FAILED){
-     printf("Mapping Failed\n");
-     return 1;
+    int mid =( ARRAY_SIZE-1) /2;
+    int end = ARRAY_SIZE - 1;
+    if ( ARRAY_SIZE <= 8 )
+    {
+        merge_sort(0, end);
+        printArray(arr , ARRAY_SIZE);
+        shmctl(shmid, IPC_RMID, &item);
     }
+    pid = fork();
+    if (pid ==0){ // merge first half of array in this child process
+        shm_atch =  shmat(shmid ,(void*)0,  SHM_RND);
+        if (shm_atch < 0){printf("error in shmat() on first fork") ; }
+        for(jj=0; jj<88888;jj++){}
+        merge_sort(0, mid );
+        }
+
+    else{
+        pid2 = fork();
+        if ( pid2 == 0){ //merge second half of array in this child process 
+            
+            shm_atch =  shmat(shmid ,(void*)0,  SHM_RND);
+            if (shm_atch < 0){printf("error in shmat() on second fork") ; }
+            for(jj=0; jj<8888;jj++){}
+            merge_sort(mid+1 , end);
+            for(jj=0; jj<8888;jj++){}
+        }
+        else{  // wait for its children proccesses and then merge two sorted parts together
+            // for (int i = 0 ; i<2 ; i++)
+            //     wait(NULL);
+            waitpid(pid ,&wait1 ,0);
+            waitpid(pid2 ,&wait2,0);
+            printf("resid be in!");
+            for(jj=0; jj<8888;jj++){}
+            for(jj=0; jj<8888;jj++){}
+            merge(0 , mid , end);
+            printArray(arr, ARRAY_SIZE);
+        }
+            for(jj=0; jj<8888;jj++){}
+            for(jj=0; jj<8888;jj++){}
+            for(jj=0; jj<8888;jj++){}
+
+    }
+
     // for (int ii =0; ii<ARRAY_SIZE+1 ; ii++){
     //     scanf("%d", &arr[ii]);
     // }
-    merge_sort(0 , ARRAY_SIZE-1);
+    // merge_sort(0 , ARRAY_SIZE-1);
     printArray(arr, ARRAY_SIZE);
     for (int ii = 0; ii < ARRAY_SIZE ; ii++){
         printf("%d\t",arr[ii]);
@@ -72,7 +125,7 @@ void merge_sort(int l , int r){
     // int middle = (l+r)/2;
     merge_sort(l, middle);
     merge_sort(middle+1, r);
-    merge(l , middle , middle+1 , r);
+    merge(l , middle  , r);
     }
     printf("merge_sort called with:(%d , %d)\n" , l,r);
     printf("***\t");
@@ -81,7 +134,8 @@ void merge_sort(int l , int r){
     printf("\n");
 }
 //------------------------------------------------------------------------------------------------
-void merge(int s1 , int e1 , int s2 , int e2) {
+void merge(int s1 , int e1 , int e2) {
+    int s2= e1+1;
     int i1=s1; 
     int i2=s2;
     // int n1 = e1-s1+1 ; 
@@ -112,7 +166,7 @@ void merge(int s1 , int e1 , int s2 , int e2) {
         arr[tmp_i] = temp_arr[tmp_i];
         printf("-> %d\n" , arr[tmp_i]);
     }
-    printf("merge called with (%d,%d,%d,%d) and k is:%d\n" , s1, e1, s2,e2 , k) ;
+    printf("merge called with (%d,%d,%d,%d) and k is:%d\n" , s1, e1, s2, e2 , k) ;
     printf("###\t");
     // printArray(arr, ARRAY_SIZE);
     for (int ii = 0; ii < ARRAY_SIZE ; ii++) printf("%d\t",arr[ii]);
