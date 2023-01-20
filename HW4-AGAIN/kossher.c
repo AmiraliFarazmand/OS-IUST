da5c2fb851b09980f6ebdc1e2b1a91339e751d4a
amirfzd <amiralifarazmand80@gmail.com>
2023-01-20 06:00:50 +0330

    kossher.c mergesort is working cur curg......../ks ./ks !

--- HW4-AGAIN/kossher.c
+++ HW4-AGAIN/kossher.c
@@ -24,11 +24,11 @@ Useful links:
 #include <sys/mman.h>
 #define ARRAY_SIZE 4
 
-int* arr ;
+int arr [4] = {4665, 5, 44 ,8} ;
      
 int * shm ;
 int  shm_seg ;
-int temp_arr[500] ;
+int temp_arr[100] ;
 void merge_sort(int l , int r);
 void merge(int s1 , int e1 , int s2 , int e2) ;
 
@@ -43,8 +43,9 @@ void printArray(int A[], int size)
 void main(){
     // int arr[ARRAY_SIZE]= {0,0,0,0} ;
     // int* arr  =(int*) malloc(ARRAY_SIZE* sizeof(int));
-    arr = (int*) mmap(NULL, sizeof(int)* ARRAY_SIZE, PROT_READ| PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, 0, 0);
+    // arr = (int*) mmap(NULL, sizeof(int)* ARRAY_SIZE, PROT_READ| PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, 0, 0);
     // if (arr=-1){printf("shit\n");return 1;}
+    printArray(arr, ARRAY_SIZE);
     int *ptr = mmap(NULL,ARRAY_SIZE*sizeof(int),
      PROT_READ | PROT_WRITE,
      MAP_SHARED | MAP_ANONYMOUS,
@@ -54,29 +55,32 @@ void main(){
      printf("Mapping Failed\n");
      return 1;
     }
-    for (int ii =0; ii<ARRAY_SIZE+1 ; ii++){
-        scanf("%d", &arr[ii]);
-    }
+    // for (int ii =0; ii<ARRAY_SIZE+1 ; ii++){
+    //     scanf("%d", &arr[ii]);
+    // }
     merge_sort(0 , ARRAY_SIZE-1);
-    for (int ii = 0; ii <= ARRAY_SIZE ; ii++){
+    printArray(arr, ARRAY_SIZE);
+    for (int ii = 0; ii < ARRAY_SIZE ; ii++){
         printf("%d\t",arr[ii]);
     }
-    printArray(arr, ARRAY_SIZE);
 }
+//=================================================================================================
 // ------------------------------------------------------------------------------------------------
 void merge_sort(int l , int r){
     if (l<r){
-    // int middle = l+ (r-l)/2 ;
-    int middle = (l+r)/2;
+    int middle = l+ (r-l)/2 ;
+    // int middle = (l+r)/2;
     merge_sort(l, middle);
     merge_sort(middle+1, r);
     merge(l , middle , middle+1 , r);
-    printf("merge_sort called with:(%d , %d)\n" , l,r);
-    printArray(arr , ARRAY_SIZE );
-    printf("***\n");
     }
+    printf("merge_sort called with:(%d , %d)\n" , l,r);
+    printf("***\t");
+    // printArray(arr , ARRAY_SIZE );
+    for (int ii = 0; ii < ARRAY_SIZE ; ii++) printf("%d\t",arr[ii]);
+    printf("\n");
 }
-
+//------------------------------------------------------------------------------------------------
 void merge(int s1 , int e1 , int s2 , int e2) {
     int i1=s1; 
     int i2=s2;
@@ -94,20 +98,23 @@ void merge(int s1 , int e1 , int s2 , int e2) {
         }
         k++ ;
     }
-    while(i1<e1){
+    while(i1<=e1){
         temp_arr[k] = arr[i1];
         i1++;
         k++;
     }
-    while(i2<e2){
+    while(i2<=e2){
         temp_arr[k] = arr[i2];
         i2++;
         k++;     
     }
-    for (int tmp_i= s1 ; tmp_i <= k ; tmp_i++){
+    for (int tmp_i= s1 ; tmp_i < k ; tmp_i++){
         arr[tmp_i] = temp_arr[tmp_i];
+        printf("-> %d\n" , arr[tmp_i]);
     }
-    printf("merge func with (%d,%d,%d,%d)\n" , s1, e1, s2,e2) ;
-    printArray(arr, ARRAY_SIZE);
-    printf("###\n");
+    printf("merge called with (%d,%d,%d,%d) and k is:%d\n" , s1, e1, s2,e2 , k) ;
+    printf("###\t");
+    // printArray(arr, ARRAY_SIZE);
+    for (int ii = 0; ii < ARRAY_SIZE ; ii++) printf("%d\t",arr[ii]);
+    printf("\n");
 }
